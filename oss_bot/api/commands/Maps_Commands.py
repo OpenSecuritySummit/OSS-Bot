0e7bd088fed53a5172ff57fd8e10a2e54b013b40
Dinis Cruz <dinis.cruz@owasp.org>
2019-06-08 10:48:23 +0100

    updated submodules and wired map's command

--- oss_bot/api/commands/Maps_Commands.py
+++ oss_bot/api/commands/Maps_Commands.py
@@ -7,6 +7,16 @@ class Maps_Commands:
         return 'pong : {0}'.format(event)
 
     @staticmethod
+    def create(*event):
+        aws_lambda = Lambda('osbot_browser.lambdas.lambda_browser')
+        params = ["maps", "exec_js"]
+        params.extend(event[2])
+
+        payload = {"params": params,
+                   'data': {'channel': event[1]}}
+        aws_lambda.invoke_async(payload)
+
+    @staticmethod
     def cup_of_tea(*event):
         aws_lambda = Lambda('osbot_browser.lambdas.lambda_browser')
         payload = {"params": ["maps", "render", "cup-of-tea"],
@@ -19,5 +29,7 @@ class Maps_Commands:
         payload = {"params": ["maps", "default"],
                    'data': {'channel': event[1]}}
         aws_lambda.invoke_async(payload)
+
+
 
 
